3a0b88ed808fca5a9fded19e0a1dd79232b113ca
echewisi <josephechewisi@gmail.com>
2023-03-04 15:56:25 +0100

    'modification'

--- member/models.py
+++ member/models.py
@@ -12,14 +12,11 @@ gender_choices= (
 class IT(models.Model):
     staff = models.OneToOneField('Staff', on_delete=models.CASCADE, primary_key= True, related_name='it_member' )
     specialization = models.CharField(max_length=100, default='unassigned')
-
     class Meta:
         verbose_name_plural= "IT"
 
     def __str__(self):
         return f"{self.staff.firstname} {self.staff.lastname} - {self.specialization}"
-    
-
 class Media(models.Model):
     staff = models.OneToOneField('Staff', on_delete=models.CASCADE, primary_key=True, related_name='media_member')
     specialization = models.CharField(max_length=100, default='unassigned')
@@ -37,25 +34,6 @@ class Admin(models.Model):
 
     def __str__(self):
         return f"{self.staff.firstname} {self.staff.lastname} - {self.specialization}"
-
-class Staff(models.Model):
-    department_choices=(
-    ("ADMIN", "Administration"),
-    ("IT", "Information Technology"),
-    ("MEDIA", "Media"),
-    )
-
-    firstname= models.CharField(max_length= 200, blank= False)
-    lastname= models.CharField(max_length= 200, blank= False)
-    staff_id= models.CharField(max_length= 100, primary_key= True, unique= True, default= 0)
-    phonenumber= PhoneNumberField()
-    email_address= models.EmailField()
-    department= models.CharField(max_length= 100, choices=department_choices, blank= False)
-    gender= models.CharField(max_length=100, choices=gender_choices)
-    state_of_origin= models.CharField(max_length= 234, blank= False)
-    account_number= models.CharField(max_length= 20, validators=[RegexValidator(r'^\d+$', 'Enter a valid account number')], blank= False)
-    bank= models.CharField(max_length= 100, blank= False)
-    account_name= models.CharField(max_length= 100, null= False, blank= False)
     
 class Staff(models.Model):
     department_choices=(
@@ -81,45 +59,23 @@ class Staff(models.Model):
         return self.staff_id
 
     def save(self, *args, **kwargs):
-        # Create corresponding IT, ADMIN or MEDIA object based on selected department
-        if self.department == "IT":
-            it_obj = IT(staff=self)
-            it_obj.save()
-        elif self.department == "ADMIN":
-            admin_obj = Admin(staff=self)
-            admin_obj.save()
-        elif self.department == "MEDIA":
-            media_obj = Media(staff=self)
-            media_obj.save()
         super().save(*args, **kwargs)
+        if self.department == "IT":
+            it_member = IT.objects.create(staff=self)
+            it_member.save()
+        elif self.department == "ADMIN":
+            admin_member = Admin.objects.create(staff=self)
+            admin_member.save()
+        elif self.department == "MEDIA":
+            media_member = Media.objects.create(staff=self)
+            media_member.save()
 
-class IT(models.Model):
-    staff = models.OneToOneField('Staff', on_delete=models.CASCADE, primary_key= True, related_name='it_member' )
-    specialization = models.CharField(max_length=100, default='unassigned')
-    class Meta:
-        verbose_name_plural= "IT"
 
-    def __str__(self):
-        return f"{self.staff.firstname} {self.staff.lastname} - {self.specialization}"
+
+
     
 
-class Media(models.Model):
-    staff = models.OneToOneField('Staff', on_delete=models.CASCADE, primary_key=True, related_name='media_member')
-    specialization = models.CharField(max_length=100, default='unassigned')
-    class Meta:
-        verbose_name_plural = "Media"
 
-    def __str__(self):
-        return f"{self.staff.firstname} {self.staff.lastname} - {self.specialization}"
-
-class Admin(models.Model):
-    staff = models.OneToOneField('Staff', on_delete=models.CASCADE, primary_key= True, related_name= 'admin_member')
-    specialization = models.CharField(max_length=100, default= 'unassigned')
-    class Meta:
-        verbose_name_plural = "Admins"
-
-    def __str__(self):
-        return f"{self.staff.firstname} {self.staff.lastname} - {self.specialization}"
     
 
 
--- member/views.py
+++ member/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from.forms import StaffForm
 
 def home(request):
     return render(request, 'base.html' )
+
+def signup(request):
+    staff_form= StaffForm()
+    if request.method== "POST":
+        staff_form= StaffForm(request.POST)
+        if staff_form.is_valid:
+            staff=staff_form.save(commit= False)
+            staff.save()
+            return redirect('home')
+    else:
+        staff_form= StaffForm()
+        
+    context={
+            'staff_form':staff_form
+        }
+    return render(request, 'base/registration.html', context)
 
 # Create your views here.
